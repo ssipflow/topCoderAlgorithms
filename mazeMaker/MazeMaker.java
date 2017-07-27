b9f4175dfd8b63cba73825dca65776c25cb7151b
ssipflow <ssipflow@gmail.com>
2017-07-27 14:46:40 +0900

    Add files via upload

---
+++ mazeMaker/MazeMaker.java
@@ -0,0 +1,54 @@
+package com.topcoder.mazeMaker;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class MazeMaker {
+
+    public int longesgPath(String[] maze, int startRow, int startCol, int[] moveRow, int[] moveCol){
+        int max = 0;
+        int width = maze[0].length(),
+                height = maze.length;
+        int[][] board = new int[height][width];
+
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++)
+                board[i][j] = -1;
+        }
+
+        board[startRow][startCol] = 0;
+
+        Queue<Integer> queueX = new LinkedList<>();
+        Queue<Integer> queueY = new LinkedList<>();
+        queueX.add(startCol);
+        queueY.add(startRow);
+
+        while(!queueX.isEmpty()){
+            int x = queueX.poll(),
+                    y = queueY.poll();
+
+            for(int i = 0; i < moveRow.length; i++){
+                int nextX = x + moveCol[i],
+                        nextY = y + moveRow[i];
+
+                if(0 <= nextX && nextX < width
+                        && 0 <= nextY && nextY < height
+                        && board[nextX][nextY] == -1
+                        && maze[nextY].charAt(nextX) == '.'){
+                    board[nextY][nextX] = board[y][x] + 1;
+                    queueX.add(nextX);
+                    queueY.add(nextY);
+                }
+            }
+        }
+
+        for(int i = 0; i < height; i++){
+            for(int j = 0; j < width; j++){
+                if(maze[i].charAt(j) == '.' && board[i][j] == -1) return -1;
+                max = Math.max(max, board[i][j]);
+            }
+        }
+
+        return max;
+    }
+}
